1357c3427b4f9b702ffd7ecaf2a793a6b1f78a27
IliDevia <neiluamaibi@mail.ru>
2024-12-13 15:36:57 +0300

    added doc strings

--- Backend/main.py
+++ Backend/main.py
@@ -1,18 +1,33 @@
+'''
+Это главный модуль бэка, он запускает веб сервер и контролирует апи
+'''
+
+
 from uvicorn import run
 from fastapi import FastAPI
 from Backend.shema import WeatherData
 from Backend.service import Service
 
 
-app = FastAPI()
+app = FastAPI()  # создание объекта приложения на веб сервере
 
 
-@app.get('/', summary='Получить прогноз')
-def get_forecast(data: WeatherData):
+@app.get('/', summary='Получить прогноз')  # endpoint. http uri к нему
+def get_forecast(data: WeatherData):  # Это принимает запросы, обрабатывает их и возвращает ответ
+    """
+    Обрабатывает HTTP GET запрос для получения прогноза на основе входящих данных о погоде.
+
+    Параметры:
+    - data (WeatherData): Объект с данными о погоде, включая средний ветер, максимальный ветер,
+      температуру, видимость и другие параметры.
+
+    Возвращает:
+    dict: Словарь с вероятностью лавины.
+    """
     forecast_value: float = Service.get_forecast(data)
-    response = {"Avalanche probability": forecast_value}
+    response: dict = {"Avalanche probability": forecast_value}
     return response
 
 
-if __name__ == '__main__':
+if __name__ == '__main__':  # Вход в программу
     run('main:app', reload=False)
--- Backend/service.py
+++ Backend/service.py
@@ -1,3 +1,11 @@
+'''
+Священный модуль сервиса.
+Он, как видно в себе собирает бизнес-логику.
+Принимает pydantic объект с запросом, переводит в список,
+запускает метод для получения прогноза и возвращает его
+'''
+
+
 from ML.ml_model import ml_model_instance
 from Backend.shema import WeatherData
 from Backend.serializer import Serializer
